fc89107f11f54b350bc0857ae093456d79e16d22
Jarett Lee <googleplex_unknown@yahoo.com>
2016-02-21 10:51:12 -0500

    change constructor
    constructor accepts multiple sizes because the size of the board should
    change with different difficulties
    the size of the board is used very often so I added it as an instance
    variable
    define the PieceType for bomb and revealed squares
    add 2d int array visibleBoard to store the number of bombs surrounding
    each square
    add generateVisibleBoard method to initialize the visibleBoard

--- BoardGame/src/Minesweeper/MinesweeperBoard.java
+++ BoardGame/src/Minesweeper/MinesweeperBoard.java
@@ -6,15 +6,71 @@ import game.PieceType;
 
 public class MinesweeperBoard extends CharBoard {
 
-	public MinesweeperBoard() {
-		super(5);
-		// TODO Auto-generated constructor stub
+	/** This character represents a bomb */
+	public static final PieceType BOMB = PieceType.X;
+	/** This character represents a revealed square */
+	public static final PieceType OPEN = PieceType.O;
+
+	/** This 2d array shows the user the number of bombs around each square */
+	private int[][] visibleBoard;
+	/**
+	 * This board keeps track of the size of the board because the value is used
+	 * often
+	 */
+	private int size;
+
+	// The previous board tracks the revealed squares and the squares with bombs
+	// on them
+
+	public MinesweeperBoard(int size) {
+		super(size);
+		visibleBoard = new int[size][size];
+		this.size = size;
+		generateVisibleBoard();
 	}
 
+	public void generateVisibleBoard() {
+		for (int j = 0; j < size; j++) {
+			for (int i = 0; i < size; i++) {
+
+				if (getBoard()[j][i] == BOMB.getChar())
+					visibleBoard[j][i] = 0;
+				else
+					visibleBoard[j][i] = numOfSurroundingBombs(j, i);
+			}
+		}
+	}
+	
+	public int numOfSurroundingBombs(int y, int x) {
+		// TODO
+		return 0;
+	}
+	// (1, 1)
+	// (1, 0)
+	// (1, -1)
+	//
+	// (0, 1)
+	// (0, -1)
+	// 
+	// (-1, -1)
+	// (-1, 0)
+	// (-1, 1)
+
+
+	/**
+	 * This method verifies that a piece can be placed on this square. For
+	 * Minesweeper, this involves checking if the square had been reveled
+	 * before.
+	 * 
+	 * @param piece
+	 *            the piece that will be checked
+	 * @param c
+	 *            the coordinates the piece will be checked on
+	 * @return if the piece can be placed on the coordinate
+	 */
 	@Override
 	public boolean canPlace(PieceType piece, Coordinate c) {
-		// TODO Auto-generated method stub
-		return false;
+		return isEmpty(c);
 	}
 
 	@Override
