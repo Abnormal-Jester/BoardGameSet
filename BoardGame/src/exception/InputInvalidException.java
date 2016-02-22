83125c48e6a37e37a335e445f1788f363e198585
Jarett Lee <googleplex_unknown@yahoo.com>
2016-02-21 20:53:26 -0500

    update exception comments

--- BoardGame/src/exception/InputInvalidException.java
+++ BoardGame/src/exception/InputInvalidException.java
@@ -1,5 +1,8 @@
 package exception;
 
+/**
+ * This Exception triggers during runtime when the user inputs an invalid input.
+ */
 public class InputInvalidException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
--- BoardGame/src/exception/PlacementFailedException.java
+++ BoardGame/src/exception/PlacementFailedException.java
@@ -1,9 +1,11 @@
 package exception;
 
+/**
+ * This exception occurs when the the board fails to place a piece where the
+ * user indicated.
+ */
 public class PlacementFailedException extends RuntimeException {
-	/**
-	 * 
-	 */
+	/** Default serial Version UID */
 	private static final long serialVersionUID = 1L;
 
 	public PlacementFailedException() {
