4105b53cbb3c53f6c6c595d1ba86f0a70cfebd59
Jarett Lee <googleplex_unknown@yahoo.com>
2016-02-25 10:06:12 -0500

    make Square
    replace the char in CharBoard

---
+++ BoardGame/src/game/Square.java
@@ -0,0 +1,19 @@
+package game;
+
+public class Square {
+	char visibleChar;
+	
+	
+	public Square (char visibleChar) {
+		setChar(visibleChar);
+	}
+	
+	public char getChar() {
+		return visibleChar;
+	}
+	
+	public void setChar(char visibleChar) {
+		this.visibleChar = visibleChar;
+	}
+	
+}
